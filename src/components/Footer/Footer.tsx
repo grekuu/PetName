5a0f42def8233001bc0024ab62f44b1c0a674d4f
grekuu <gracjanprusik@gmail.com>
2023-08-14 19:23:34 +0200

    feat(Footer): Implement and finalize footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,20 @@
-import "./footer.scss";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+import './footer.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <Navbar fixed="bottom" expand="lg" bg="primary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Text className="copyright-text">
+          Copyright © {currentYear} <a href="https://www.imiedlazwierzaka.org">ImieDlaZwierzaka.org</a>
+        </Navbar.Text>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Footer;
